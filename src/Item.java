8f62bd6ce742af96cbb67cf5c9a6549e6dce3a9e
DongerKai <koekchan@gmail.com>
2018-07-19 15:31:54 +0800

    初始化提交

---
+++ src/Item.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Item{
+	/*控制台读取输出*/
+	/*public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		System.out.println("第一个" + a);
+		int b = s.nextInt();
+		System.out.println(b);
+	}*/
+	
+	/*example*/
+	
+}

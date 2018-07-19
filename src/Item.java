7ae108a60fcdb7f84f135218cf3baf473d549521
DongerKai <koekchan@gmail.com>
2018-07-19 16:52:35 +0800

    add some details

--- src/Item.java
+++ src/Item.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 public class Item{
 	/*控制台读取输出*/
-	/*public static void main(String[] args) {
+	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		int a = s.nextInt();
 		System.out.println("第一个" + a);
 		int b = s.nextInt();
 		System.out.println(b);
-	}*/
+	}
 	
 	/*example*/
 	
